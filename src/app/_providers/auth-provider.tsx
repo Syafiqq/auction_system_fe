0f3ccc9d1f8f5517c358714f723b20b9c89023ee
msyafiqq <muhammad.syafiq@geniebook.com>
2024-08-13 09:37:38 +0700

    003: Auth, Add login provider

---
+++ src/app/_providers/auth-provider.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import React, {createContext, useContext, useEffect, useState} from 'react';
+import {useRouter} from 'next/navigation';
+import authRepository from "@/data/repository/auth-repository";
+import {User} from "@/domain/definition/entity/user.definition";
+import logoutUseCase from "@/domain/usecase/logout-use-case";
+
+const AuthContext = createContext<{
+    user: User | null;
+    login: () => void;
+    logout: () => void;
+} | null>(null);
+
+interface AuthProviderProps {
+    children: React.ReactNode;
+}
+
+export function AuthProvider({children}: AuthProviderProps) {
+    const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        const token = authRepository.loadToken();
+
+        if (!token) {
+            router.push('/auth');
+            setLoading(false);
+            return;
+        }
+
+        const loadUserProfile = () => {
+            try {
+                const user = authRepository.loadProfile();
+                if (user === null) {
+                    router.push('/auth');
+                } else {
+                    setUser(user);
+                }
+            } catch (error) {
+                router.push('/auth');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        loadUserProfile();
+    }, [router]);
+
+    const login = () => {
+        const user = authRepository.loadProfile();
+        if (user) {
+            setUser(user);
+        }
+    };
+
+    const logout = () => {
+        logoutUseCase.execute()
+        setUser(null);
+        router.push('/login');
+    };
+
+    if (loading) return <p>Loading...</p>;
+
+    return (
+        <AuthContext.Provider value={{user, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import {Inter} from "next/font/google";
 import "./globals.css";
 import React from "react";
 import ToastProvider from "@/app/_toastify/toast-provider";
+import {AuthProvider} from "@/app/_providers/auth-provider";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -20,7 +21,9 @@ export default function RootLayout({
         <html lang="en">
         <body className={inter.className}>
         <ToastProvider>
-            {children}
+            <AuthProvider>
+                {children}
+            </AuthProvider>
         </ToastProvider>
         </body>
         </html>
