08781b6d4dc7754effce68a388f4eacca4c358fe
msyafiqq <muhammad.syafiq@geniebook.com>
2024-08-13 17:24:14 +0700

    005: Dashboard, Add search form

---
+++ src/app/dashboard/components/search-form.tsx
@@ -0,0 +1,101 @@
+'use client';
+
+import React, {useEffect, useState} from 'react';
+import {useRouter} from 'next/navigation';
+
+const SearchForm = ({requestFetchData, queryString}: {
+    requestFetchData: (params: URLSearchParams) => void,
+    queryString: URLSearchParams,
+}) => {
+    const router = useRouter();
+
+    // Mapping values to display labels
+    const sortPriceOptions = {
+        '': 'Not sorted',
+        asc: 'Lowest first',
+        desc: 'Highest first',
+    };
+
+    // Reverse mapping for initial state
+    const reverseSortPriceOptions = {
+        'Not sorted': '',
+        'Lowest first': 'asc',
+        'Highest first': 'desc',
+    };
+
+    const [name, setName] = useState(queryString.get('name') || '');
+    const [description, setDescription] = useState(queryString.get('description') || '');
+    const [sortPrice, setSortPrice] = useState(queryString.get('sort_price') || '');
+
+    useEffect(() => {
+        if (name) queryString.set('name', name);
+        else queryString.delete('name');
+
+        if (description) queryString.set('description', description);
+        else queryString.delete('description');
+
+        if (sortPrice) {
+            queryString.set('sort_price', sortPrice);
+        } else {
+            queryString.delete('sort_price');
+        }
+
+        router.replace(`?${queryString.toString()}`);
+    }, [name, description, sortPrice]);
+
+    const handleSearch = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        requestFetchData(queryString);
+    };
+
+    return (
+        <div className="container mx-auto py-8">
+            <form className="space-y-4" onSubmit={handleSearch}>
+                <div>
+                    <label className="block text-gray-700">Name:</label>
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        className="w-full px-4 py-2 border rounded-md"
+                        placeholder="Enter name"
+                    />
+                </div>
+                <div>
+                    <label className="block text-gray-700">Description:</label>
+                    <input
+                        type="text"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        className="w-full px-4 py-2 border rounded-md"
+                        placeholder="Enter description"
+                    />
+                </div>
+                <div>
+                    <label className="block text-gray-700">Sort by Price:</label>
+                    <select
+                        value={sortPrice}
+                        onChange={(e) => setSortPrice(e.target.value)}
+                        className="w-full px-4 py-2 border rounded-md"
+                    >
+                        {Object.entries(sortPriceOptions).map(([value, label]) => (
+                            <option key={value} value={value}>
+                                {label}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <button
+                    type="submit"
+                    className="w-full py-2 bg-blue-500 text-white rounded-md"
+
+                >
+                    Search
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default SearchForm
