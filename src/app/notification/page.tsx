24d7ed4109ea28434669fbb1b1137de700c9dbf7
msyafiqq <muhammad.syafiq@geniebook.com>
2024-08-14 09:05:08 +0700

    011: InAppNotif, Add in app notif page

---
+++ src/app/notification/page.tsx
@@ -0,0 +1,7 @@
+import InAppNotificationList from "@/app/notification/components/in-app-notification";
+
+export default function DashboardPage() {
+    return (
+        <InAppNotificationList></InAppNotificationList>
+    );
+}
